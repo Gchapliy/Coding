46decf4413d5237c7fefe389e2518a94bbe3b3a3
Hryhorii <grishachapliy1@gmail.com>
2025-04-03 21:32:13 +0300

    1. LongestValidParenthesis - https://leetcode.com/problems/longest-valid-parentheses/description/

---
+++ src/leetcode/LongestValidParenthesis.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class LongestValidParenthesis {
+    public static void main(String[] args) {
+        System.out.println(longestValidParentheses("(()") + " -> 2");
+        System.out.println(longestValidParentheses(")()())") + " -> 4");
+        System.out.println(longestValidParentheses("()(())") + " -> 6");
+        System.out.println(longestValidParentheses("((()))())") + " -> 8");
+        System.out.println(longestValidParentheses("()(()") + " -> 2");
+        System.out.println(longestValidParentheses(")()())()()(") + " -> 4");
+        System.out.println(longestValidParentheses(""));
+    }
+
+    public static int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+        return maxLength;
+    }
+}
